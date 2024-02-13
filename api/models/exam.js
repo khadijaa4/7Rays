f39afc485f3ca63b83515a6395ad80167641c7c1
giordanorogers <giordanopaulrogers@gmail.com>
2024-02-13 16:44:01 -0500

    Some api fixes. Tested endpoints w/ postman. Everythings working

--- api/models/exam.js
+++ api/models/exam.js
@@ -1,3 +1,21 @@
 // Defines an exam model
 // Specifies the structure of an exam document in the MondoDB database
 // Integrate the exam model with the exam controller
+
+const mongoose = require('mongoose');
+
+const examSchema = new mongoose.Schema({
+    PATIENT_ID: String,
+    AGE: Number,
+    SEX: String,
+    ZIP: Number,
+    LATEST_BMI: Number,
+    LATEST_WEIGHT: Number,
+    png_filename: String,
+    exam_Id: String,
+    ICU_Admit: String,
+    ICU_admits: Number,
+    MORTALITY: String
+});
+
+module.exports = mongoose.model('Exam', examSchema);
