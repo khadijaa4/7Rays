6f936009d9b7adc450f15dce3bc8c3dceffe269d
nmcgregor15 <68406747+nmcgregor15@users.noreply.github.com>
2024-02-21 00:12:40 -0500

    reset main
    hard reseting main branch with backend files

--- api/app.js
+++ api/app.js
@@ -5,8 +5,8 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var cors = require('cors');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+
+var examsRouter = require('../routes/exams-route');
 
 var app = express();
 
@@ -17,8 +17,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+
+app.use('/exams-route', examsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
